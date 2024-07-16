941989925b75b31377da3f7eb9d67bc3c4c85dd9
surawutsukkum <surawut@live.com>
2024-07-16 14:17:20 +0700

    test

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pandas as pd
 #linking df to our dataset
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 report = ProfileReport(df)
+report.to_notebook_iframe()
+report.to_file('file_name')
+report.to_html()
 
 
 
