541dcd94fcc55116ffb8ba962574740b4fb0baaa
surawutsukkum <surawut@live.com>
2024-07-16 14:44:23 +0700

    Test

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-#newfile
-#importing modules
+# importing packages
 import pandas as pd
+from ydata_profiling import ProfileReport
+
 #linking df to our dataset
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-df.describe(include='all')
-
-
-
-
+report = ProfileReport(df)
+report.to_notebook_iframe()
