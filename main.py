922f5292752b37101be9e8892238ee35593ec91f
surawutsukkum <surawut@live.com>
2024-07-16 13:48:51 +0700

    test

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pandas
 #importing modules
 from pandas_profiling import ProfileReport
 import pandas as pd
+
+#linking df to our dataset
+df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
