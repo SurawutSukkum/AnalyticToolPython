91535e6e7defc533d07a68649fe0a0b245175d38
surawutsukkum <surawut@live.com>
2024-07-16 13:53:35 +0700

    bb

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 #importing modules
 from pandas_profiling import ProfileReport
 import pandas as pd
-
 #linking df to our dataset
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-
 report = ProfileReport(df)
 
-report.to_notebook_iframe()
+
 
